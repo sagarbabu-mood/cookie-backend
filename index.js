15dc9583a0c90a83559efeb2e3dc6afce8f022bf
Sagar Babu <sagarbabusv180@gmail.com>
2024-07-30 04:00:38 +0530

    url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const databasePath = path.join(__dirname, "nxtwatch.db");
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true, }));
+app.use(cors({ origin: 'https://cookie-frontend.onrender.com', credentials: true, }));
 app.use(cookieParser()); // Add this line
 app.use(express.urlencoded({ extended: true }));
 
